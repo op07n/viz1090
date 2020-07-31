67f6f6e53662c15b56131e758644b775fb2ea3da
op07n <39182105+op07n@users.noreply.github.com>
2020-07-31 11:37:01 +0200

    Update winstubs.h

--- winstubs.h
+++ winstubs.h
@@ -1,10 +1,11 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <windows.h>
 #include <fcntl.h>
 #include <io.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
 #include <direct.h>
 #include <stdint.h>
 #include <pthread.h>
 #include <cstdio>
 #define usleep(x) Sleep(x/1000)
+#define sleep(x) Sleep(x)
